91ec2015bb1f6369165a4286d0c9c81767da5aa7
Reza <reza.sh44@gmail.com>
2023-09-11 21:18:39 -0400

    Updated Color

--- scripts/color.js
+++ scripts/color.js
@@ -1,106 +1,202 @@
-var margin = { top: 20, right: 20, bottom: 20, left: 20 };
-var width = 400;
-var height = 350;
+var ID;
+var Value;
+var radio1;
+var radio2;
+var results_json = [];
+var current_elem = 0;
+var array_elem1 = 0;
+var array_elem2 = 0;
+var array_elem3 = 0;
+var motion_type = [1, 2, 3];
+var move_true;
+var count;
+var padding = 10;
+var timer_ret_val = false;
 
-var dataXRange = { min: 0, max: 100 };
-var dataYRange = { min: 0, max: 100 };
-var xAxisLabelHeader = "X Header";
-var yAxisLabelHeader = "Y Header";
+var margin = {
+  top: 18,
+  left: 18,
+  bottom: 18,
+  right: 18
+};
 
-var data;
-var chart;
-var chartWidth;
-var chartHeight;
-var colors = ["#ff0000", "#00ff00", "#0000ff","#808080"];
+var chartWidth = document.getElementById("chartContainer").offsetWidth - margin.left - margin.right;
+var chartHeight = document.getElementById("chartContainer").offsetHeight - margin.top - margin.bottom;
 
-init(drawDots);
+var svg = d3
+  .select("#chart")
+  .append("svg")
+  .attr("width", chartWidth)
+  .attr("height", chartHeight)
+  .append("g")
+  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-function init(drawChart) {
-  console.log("initiating!");
-  chartWidth = width - margin.left - margin.right;
-  chartHeight = height - margin.top - margin.bottom;
+var display1 = d3.select("#chart");
+var display2 = d3.select("#test");
+var display3 = d3.select("#ranger");
+var display4 = d3.select("#last");
+var display5 = d3.select("#notice");
+// Added by Shae
+var display6 = d3.select("#test_difficulty");
+var display7 = d3.select("#ranger_difficulty");
 
-  var N = 2; // Change N to the desired size of the grid
+var N = 10; // Change N to the desired size of the grid
 
-  data = [];
+motion4(array_elem3, N); // Pass the N value to the function
 
-  initializeChart();
-  createAxes();
-  drawChart(N);
-} //end init
+function motion4(num, N) {
+  var circle_data_2 = [];
+  var box_data_2 = [];
 
-function initializeChart() {
-  chart = d3
-    .select("#chart")
-    .append("svg")
-    .attr("width", width)
-    .attr("height", height);
+  var gridWidth = chartWidth - margin.left - margin.right; // Width of the grid area
+  var gridHeight = chartHeight - margin.top - margin.bottom; // Height of the grid area
 
-  chart.plotArea = chart
-    .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-}
+  var maxCellSize = Math.min(gridWidth, gridHeight) / 10; // Determine the maximum cell size for N = 10
+  var cellSize = maxCellSize; // Use this as the fixed cell size for all values of N
 
-function createAxes() {
-  // x axis
-  chart.xScale = d3
-    .scaleLinear()
-    .domain([dataXRange.min, dataXRange.max])
-    .range([0, chartWidth]);
+  var totalGridWidth = cellSize * N;
+  var totalGridHeight = cellSize * N;
 
-  chart.xAxis = d3
-    .axisBottom()
-    .tickSize(0)
-    .scale(chart.xScale);
+  var translateX = (chartWidth - totalGridWidth) / 2;
+  var translateY = (chartHeight - totalGridHeight) / 2;
+  svg.attr("transform", "translate(" + translateX + "," + translateY + ")");
 
-  // y axis labels
-  chart.yScale = d3
-    .scaleLinear()
-    .domain([dataYRange.min, dataYRange.max])
-    .range([chartHeight, 0]);
-
-  chart.yAxis = d3
-    .axisLeft()
-    .tickSize(0)
-    .scale(chart.yScale);
-}
-
-function drawDots(N) {
-  var cellSize = Math.min(chartWidth, chartHeight) / N; // Size of each grid cell
-  var circleRadius = cellSize / 2.5; // Adjust the fraction to control the circle size
-
-  // Generate data for each cell in the grid
+  // Generate circle and box data based on grid size N
   for (var i = 0; i < N; i++) {
     for (var j = 0; j < N; j++) {
       var cx = (j + 0.5) * cellSize; // X position of circle center
       var cy = (i + 0.5) * cellSize; // Y position of circle center
 
-      var color = colors[(i * N + j) % colors.length]; // Assign color from the array based on the cell index
+      var radius = cellSize * 1; // Adjust the radius based on cell size
 
-      chart.plotArea
-        .append("circle")
-        .attr("class", "dot")
-        .attr("cx", cx)
-        .attr("cy", cy)
-        .attr("r", circleRadius)
-        .attr("fill", color)
-        .on("click", function() {
-          var circle = d3.select(this);
-          var id = circle.data()[0].id;
-          var cx = circle.attr("cx");
-          var cy = circle.attr("cy");
-          console.log("circle: ", id, ", ", cx, ", ", cy);
-          // Perform action on circle click
-        });
+      var colors = ["#ff0000", "#00ff00", "#0000ff", "#808080"]; // Add more colors if needed
 
-      chart.plotArea
-        .append("text")
-        .attr("class", "label")
-        .attr("x", cx)
-        .attr("y", cy)
-        .attr("text-anchor", "middle") // Set the text anchor to the middle
-        .attr("dy", "0.35em") // Adjust the dy value for vertical alignment
-        .text(i * N + j);
+      var Values = []
+      var value = Values[i * N + j]; // Get the value from the array based on the circle's index
+
+      circle_data_2.push({
+        x: cx,
+        y: cy,
+        id: i * N + j + 1,
+        r_diff: 0.13,
+        radius: cellSize / 2,  // Set a constant value
+        move: 0,
+        value: value
+      });
+
+      box_data_2.push({
+        x: j * cellSize,
+        y: i * cellSize,
+        h: cellSize,
+        w: cellSize
+      });
     }
   }
+
+  // Create arrow markers
+  svg
+    .append("defs")
+    .append("marker")
+    .attr("id", "arrowhead")
+    .attr("refX", 0) // Set refX to half of the cell size
+    .attr("refY", 3)
+    .attr("markerWidth", 30)
+    .attr("markerHeight", 30)
+    .attr("orient", "auto")
+    .append("path")
+    .attr("d", "M 0,0 V 6 L9,3 Z")
+    .attr("fill", "black");
+
+  // Function to add an arrow to a specific cell
+  function addArrow(row, col) {
+    var cellSize = maxCellSize ;
+    var cx = (col + 0.5) * cellSize;
+    var cy = (row + 0.5) * cellSize;
+    var circleRadius = circle_data_2[row * N + col].radius; // Get the circle's radius
+
+    // Define the endpoint for the arrow (the middle of the outside border)
+    var arrowStartX, arrowStartY, arrowEndX, arrowEndY;
+
+    if (col === 0) {
+      arrowStartX = -cellSize / 2;
+      arrowEndX = cx - circleRadius - 5; // Adjusted to avoid overlap with circle
+  } else if (col === N - 1) {
+      arrowStartX = totalGridWidth + cellSize / 2;
+      arrowEndX = cx + circleRadius + 5; // Adjusted to avoid overlap with circle
+  } else {
+      arrowStartX = arrowEndX = cx;
+  }
+
+  if (row === 0) {
+      arrowStartY = -cellSize / 2;
+      arrowEndY = cy - circleRadius - 5; // Adjusted to avoid overlap with circle
+  } else if (row === N - 1) {
+      arrowStartY = totalGridHeight + cellSize / 2;
+      arrowEndY = cy + circleRadius + 5; // Adjusted to avoid overlap with circle
+  } else {
+      arrowStartY = arrowEndY = cy;
+  }
+
+
+    var box_2 = svg
+    .selectAll(".rect")
+    .data(box_data_2)
+    .enter()
+    .append("rect")
+    .attr("class", "rect")
+    .style("fill", "white")
+    .attr("stroke", "black")
+    .attr("stroke-width", 0.5)
+    .attr("x", function(d) {
+      return d.x;
+    })
+    .attr("y", function(d) {
+      return d.y;
+    })
+    .attr("height", function(d) {
+      return d.h;
+    })
+    .attr("width", function(d) {
+      return d.w;
+    });
+
+    var circles_2 = svg
+    .selectAll(".circle")
+    .data(circle_data_2)
+    .enter()
+    .append("circle")
+    .attr("class", "circle")
+    .attr("r", 30) // Set the fixed radius value to 8
+    .attr("fill", function(d, i) {
+      return colors[i % colors.length];
+    })
+    .attr("cx", function(d) {
+      return d.x;
+    })
+    .attr("cy", function(d) {
+      return d.y;
+    });
+
+
+
+    // Add an arrow line
+    svg
+        .append("line")
+        .attr("x1", arrowStartX)
+        .attr("y1", arrowStartY)
+        .attr("x2", arrowEndX)
+        .attr("y2", arrowEndY)
+        .attr("stroke", "black")
+        .attr("stroke-width", 1.5)
+        .attr("marker-end", "url(#arrowhead)");
+}
+
+  // Add arrows to cells [0, 2] and [2, 2]
+  addArrow(0, 2);
+  addArrow(4, 0);
+
+  
+
+  move_true = true;
+  console.log("area");
 }
