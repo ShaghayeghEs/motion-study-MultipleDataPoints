b8c61e6eb2c2178470d7cfce75caf5dd42eb1d00
Reza <reza.sh44@gmail.com>
2023-09-12 17:03:36 -0400

    Updated vibrate

--- scripts/vibrate.js
+++ scripts/vibrate.js
@@ -1,102 +1,240 @@
-var N = 3; // Change N to the desired size of the grid
+var ID;
+var Value;
+var radio1;
+var radio2;
+var results_json = [];
+var current_elem = 0;
+var array_elem1 = 0;
+var array_elem2 = 0;
+var array_elem3 = 0;
+var motion_type = [1, 2, 3];
+var move_true;
+var count;
+var padding = 10;
+var timer_ret_val = false;
 
-var margin = { top: 20, right: 20, bottom: 20, left: 20 };
-var width = 500;
-var height = 350;
-var chartWidth = width - margin.left - margin.right;
-var chartHeight = height - margin.top - margin.bottom;
+var speeds = [100,200,300,400,1000,100,200,300,400,1000,100,200,300,400,1000,100,200,300,400,1000,100,200,300,400,1000,100,200,300,400,1000 ]; // Define the speeds array (you can adjust these values)
 
-// Calculate the maximum number of circles that can fit within the available space
-var maxCircles = Math.min(chartWidth, chartHeight) / Math.max(N, 1);
+var margin = {
+  top: 18,
+  left: 18,
+  bottom: 18,
+  right: 18
+};
 
-var circleRadius = maxCircles * 0.3; // Adjust the fraction to control the circle size
-var circleSpacing = maxCircles * 1.1; // Adjust the fraction to control the spacing between circles
 
-speedArray=[200, 400, 800, 1600, 3200, 3200,200,200,200] // Set the speed, Lower = faster
+var chartWidth = document.getElementById("chartContainer").offsetWidth - margin.left - margin.right;
+var chartHeight = document.getElementById("chartContainer").offsetHeight - margin.top - margin.bottom;
 
-var data = [];
+var svg = d3
+  .select("#chart")
+  .append("svg")
+  .attr("width", chartWidth)
+  .attr("height", chartHeight)
+  .append("g")
+  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-var chart;
+var display1 = d3.select("#chart");
+var display2 = d3.select("#test");
+var display3 = d3.select("#ranger");
+var display4 = d3.select("#last");
+var display5 = d3.select("#notice");
+// Added by Shae
+var display6 = d3.select("#test_difficulty");
+var display7 = d3.select("#ranger_difficulty");
 
-var svg = d3.select("svg");
+var N = 5; // Change N to the desired size of the grid
 
-// Calculate the required dimensions for the SVG container based on the grid size
-var containerWidth = N * circleSpacing + margin.left + margin.right;
-var containerHeight = N * circleSpacing + margin.top + margin.bottom;
+motion4(array_elem3, N); // Pass the N value to the function
 
-svg.attr("width", containerWidth).attr("height", containerHeight);
+var circles_2; // Declare circles_2 as a global variable
+var borders; // Declare borders as a global variable to maintain cell borders
 
-// Generate data for each cell in the grid
-for (var i = 0; i < N; i++) {
-  for (var j = 0; j < N; j++) {
-    var id = i * N + j;
-    var xVal = margin.left + (j +0.2) * circleSpacing; // X position of circle center
-    var yVal = margin.top + (i + 0.5) * circleSpacing; // Y position of circle center
-    var speed = speedArray[i*N+j]  ; // Get the speed value from the speedArray
-    
-    data.push({
-      id: id.toString(),
-      item: (id + 1).toString(),
-      xVal: xVal.toString(),
-      yVal: yVal.toString(),
-      speed: speed
-    });
+function motion4(num, N) {
+  var circle_data_2 = [];
+  var box_data_2 = [];
+
+  var gridWidth = chartWidth - margin.left - margin.right; // Width of the grid area
+  var gridHeight = chartHeight - margin.top - margin.bottom; // Height of the grid area
+
+  var maxCellSize = Math.min(gridWidth, gridHeight) / 10; // Determine the maximum cell size for N = 10
+  var cellSize = maxCellSize; // Use this as the fixed cell size for all values of N
+
+  var totalGridWidth = cellSize * N;
+  var totalGridHeight = cellSize * N;
+
+  var translateX = (chartWidth - totalGridWidth) / 2;
+  var translateY = (chartHeight - totalGridHeight) / 2;
+  svg.attr("transform", "translate(" + translateX + "," + translateY + ")");
+
+  // Generate circle data based on grid size N
+  for (var i = 0; i < N; i++) {
+    for (var j = 0; j < N; j++) {
+      var cx = (j + 0.5) * cellSize; // X position of circle center
+      var cy = (i + 0.5) * cellSize; // Y position of circle center
+
+      var colors = ["#008fb3"]; // Add more colors if needed
+
+      circle_data_2.push({
+        x: cx,
+        y: cy,
+        id: i * N + j + 1,
+        r_diff: 0.13,
+        currentRadius: cellSize / 5,
+        move: 0,
+        color: colors[i % colors.length], // Store the color for each circle
+        speed: speeds[(i * N + j) ]  // Assigning speed to each circle
+      });
+
+      box_data_2.push({
+        x: j * cellSize,
+        y: i * cellSize,
+        h: cellSize,
+        w: cellSize
+      });
+    }
   }
-}
 
-var circle = svg.selectAll("circle").data(data, function(d) {
-  return d;
-});
+  // Create borders for cells
+  borders = svg
+    .selectAll(".rect")
+    .data(box_data_2)
+    .enter()
+    .append("rect")
+    .attr("class", "rect")
+    .style("fill", "none")
+    .attr("stroke", "black")
+    .attr("stroke-width", 0.5)
+    .attr("x", function (d) {
+      return d.x;
+    })
+    .attr("y", function (d) {
+      return d.y;
+    })
+    .attr("height", function (d) {
+      return d.h;
+    })
+    .attr("width", function (d) {
+      return d.w;
+    });
 
-circle
-  .enter()
-  .append("circle")
-  .attr("cy", function(d) {
-    return d.yVal;
-  })
-  .attr("cx", function(d) {
-    return d.xVal;
-  })
-  .attr("r", circleRadius)
-  .attr("fill", "#008fb3")
-  .attr("speed", function(d) {
-    return d.speed;
+  circles_2 = svg
+    .selectAll(".circle") // Assign to the global variable
+    .data(circle_data_2)
+    .enter()
+    .append("circle")
+    .attr("class", "circle")
+    .attr("r", 25) // Set the fixed radius value to 8
+    .attr("fill", function (d) {
+      return d.color; // Use the stored color value
+    })
+    .attr("cx", function (d) {
+      return d.x;
+    })
+    .attr("cy", function (d) {
+      return d.y;
+    });
+
+  // Create arrow markers
+  svg
+    .append("defs")
+    .append("marker")
+    .attr("id", "arrowhead")
+    .attr("refX", 0) // Set refX to half of the cell size
+    .attr("refY", 3)
+    .attr("markerWidth", 30)
+    .attr("markerHeight", 30)
+    .attr("orient", "auto")
+    .append("path")
+    .attr("d", "M 0,0 V 6 L9,3 Z")
+    .attr("fill", "black");
+
+  // Function to add an arrow to a specific cell
+  function addArrow(row, col) {
+    var cellSize = maxCellSize;
+    var cx = (col + 0.5) * cellSize;
+    var cy = (row + 0.5) * cellSize;
+    var circleRadius = circle_data_2[row * N + col].currentRadius ; // Get the circle's current radius
+
+    // Define the endpoint for the arrow (the middle of the outside border)
+    var arrowStartX, arrowStartY, arrowEndX, arrowEndY;
+
+    if (col === 0) {
+      arrowStartX = -cellSize / 2;
+      arrowEndX = cx - circleRadius - 15; // Adjusted to avoid overlap with circle
+    } else if (col === N - 1) {
+      arrowStartX = totalGridWidth + cellSize / 2;
+      arrowEndX = cx + circleRadius + 15; // Adjusted to avoid overlap with circle
+    } else {
+      arrowStartX = arrowEndX = cx;
+    }
+
+    if (row === 0) {
+      arrowStartY = -cellSize / 2;
+      arrowEndY = cy - circleRadius - 15; // Adjusted to avoid overlap with circle
+    } else if (row === N - 1) {
+      arrowStartY = totalGridHeight + cellSize / 2;
+      arrowEndY = cy + circleRadius + 15; // Adjusted to avoid overlap with circle
+    } else {
+      arrowStartY = arrowEndY = cy;
+    }
+
+    // Add an arrow line
+    svg
+      .append("line")
+      .attr("x1", arrowStartX)
+      .attr("y1", arrowStartY)
+      .attr("x2", arrowEndX)
+      .attr("y2", arrowEndY)
+      .attr("stroke", "black")
+      .attr("stroke-width", 1.5)
+      .attr("marker-end", "url(#arrowhead)");
+  }
+
+  // Add arrows to cells [0, 2] and [2, 0]
+  addArrow(0, 2);
+  addArrow(1, 0);
+
+  move_true = true;
+  console.log("area");
+
+  // Initialize the "expanding" property for circles
+  circles_2.each(function (d) {
+    d.expanding = true;
   });
 
-vibrate();
+  // Start the vibration animation
+  vibrate();
+}
+
+// Function to start the vibration animation
+function startVibration() {
+  // Set the interval to continuously vibrate the circles
+  setInterval(function () {
+    vibrate();
+  }, Math.max(...speeds)); // Adjust the interval as needed for your desired speed
+
+  // Call vibrate initially to start the animation
+  vibrate();
+}
 
 function vibrate() {
-  circles = svg.selectAll("circle");
-  circles.transition().on("start", function repeat() {
+  circles_2.transition().on("start", function repeat() {
     d3.active(this)
       .attr("cx", function(d) {
-        return (parseInt(d.xVal, 10) + circleRadius).toString();
+        return d.x + 10; // Move the circle 10 units right
       })
       .transition()
       .duration(function(d) {
         return d.speed;
       })
       .attr("cx", function(d) {
-        return d.xVal;
+        return d.x -10; // Move the circle back to its original position
       })
       .transition()
+      .duration(function(d) {
+        return d.speed;
+      })
       .on("start", repeat);
   });
 }
-
-svg
-  .selectAll(".text")
-  .data(data)
-  .enter()
-  .append("text")
-  .attr("y", function(d) {
-    return parseInt(d.yVal, 10) + circleRadius + 4; // Adjust the offset to prevent overlapping
-  })
-  .attr("class", "label")
-  .attr("x", function(d) {
-    return d.xVal;
-  })
-  .attr("text-anchor", "middle")
-  .text(function(d) {
-    return d.item;
-  });
