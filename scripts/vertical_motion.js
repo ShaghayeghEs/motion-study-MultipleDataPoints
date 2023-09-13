3bfe3dd862f6c7d2f3506983338f88754803f442
Reza <reza.sh44@gmail.com>
2023-09-13 15:39:51 -0400

    Updated Vertical Motion!

--- scripts/vertical_motion.js
+++ scripts/vertical_motion.js
@@ -10,24 +10,26 @@ var array_elem3 = 0;
 var motion_type = [1, 2, 3];
 var move_true;
 var count;
-
-var width = 450;
-var height = 350;
-var padding = 0;
+var padding = 10;
 var timer_ret_val = false;
 
+var speeds = [1,2,3,4,5,6,8,10,12,14,16]; // Define the speeds array (you can adjust these values)
+
 var margin = {
-  top: 20,
-  left: 20,
-  bottom: 20,
-  right: 20
+  top: 18,
+  left: 18,
+  bottom: 18,
+  right: 18
 };
+
+var chartWidth = document.getElementById("chartContainer").offsetWidth - margin.left - margin.right;
+var chartHeight = document.getElementById("chartContainer").offsetHeight - margin.top - margin.bottom;
 
 var svg = d3
   .select("#chart")
   .append("svg")
-  .attr("width", width + margin.left + margin.right)
-  .attr("height", height + margin.top + margin.bottom)
+  .attr("width", chartWidth)
+  .attr("height", chartHeight)
   .append("g")
   .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
@@ -43,7 +45,23 @@ var display7 = d3.select("#ranger_difficulty");
 motion2(array_elem1);
 
 function motion2(num) {
-  var N = 4; // Define the value of N (between 2-10)
+  var N = 5 ;
+  var circle_data_2 = [];
+  var box_data_2 = [];
+
+  var gridWidth = chartWidth - margin.left - margin.right; // Width of the grid area
+  var gridHeight = chartHeight - margin.top - margin.bottom; // Height of the grid area
+
+  var maxCellSize = Math.min(gridWidth, gridHeight) / 10; // Determine the maximum cell size for N = 10
+  var cellSize = maxCellSize; // Use this as the fixed cell size for all values of N
+
+  var totalGridWidth = cellSize * N;
+  var totalGridHeight = cellSize * N;
+
+  var translateX = (chartWidth - totalGridWidth) / 2;
+  var translateY = (chartHeight - totalGridHeight) / 2;
+  svg.attr("transform", "translate(" + translateX + "," + translateY + ")");
+
 
   var circle1 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
   var circle2 = circle1;
@@ -52,20 +70,17 @@ function motion2(num) {
   var m_s1 = m_s / Math.max(...circle1); // Calculate the vertical movement factor dynamically
   var m_s2 = m_s0 / Math.max(...circle2); // Calculate the vertical movement factor dynamically
 
-  var radius = 10;
+  var radius = 18;
   move_true = false;
   count = 0;
 
-  var speeds = [10.1, 20.2, 30, 40.3, 50.5, 60.7, 30.4, 20.6, 10.9, 20.2, 30, 10.3, 20.5, 10.7, 30.4, 20.6, 10.9, 20.2, 30, 40.3, 50.5, 10.7, 30.4, 20.6, 10.9, 20.2, 30, 40.3, 50.5, 60.7, 30.4, 20.6, 10.9, 20.2, 30, 40.3, 50.5, 60.7, 30.4, 20.6, 10.9, 20.2, 30, 40.3, 50.5, 60.7, 30.4, 20.6, 10.9, 20.2, 30, 40.3, 50.5, 60.7, 30.4, 20.6, 10.9, 20.2, 30, 40.3, 50.5, 60.7, 30.4, 20.6, 10.9, 20.2, 30, 40.3, 50.5, 60.7, 30.4, 20.6, 10.9, 20.2, 30, 40.3, 50.5, 60.7, 30.4, 20.6, 10.9, 20.2, 30, 40.3, 50.5, 60.7, 30.4, 20.6, 10.9, 20.2, 30, 40.3, 50.5, 60.7, 30.4, 20.6, 10.9, 20.2, 30, 40.3, 50.5, 60.7, 30.4, 20.6, 10.9, 20.2, 30, 40.3, 50.5, 60.7, 30.4, 20.6, 10.9, 20.2, 30, 40.3, 50.5, 60.7, 30.4, 20.6, 10.9, 20.2, 30, 40.3, 50.5, 60.7, 30.4, 20.6, 10.9, 20.2, 30, 40.3, 50.5, 60.7, 30.4, 20.6, 10.9, 20.2, 30, 40.3, 50.5, 60.7, 30.4, 20.6, 10.9, 20.2, 30, 40.3, 50.5, 60.7, 30.4, 20.6, 10.9, 20.2, 30, 40.3, 50.5, 60.7, 30.4, 20.6, 10.9]; // Speeds array for each cell
+  var speeds = [1,2,4,8,16,32,64,128,64,32,16,8,4,2,1,1,1,1,1,1,1,1,1,1,2,4,8,16,32,64,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2,4,8,16,32,64,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2,4,8,16,32,64,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2,4,8,16,32,64,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2,4,8,16,32,64,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]; // Speeds array for each cell
 
-  var containerWidth = width - margin.left - margin.right;
-  var containerHeight = height - margin.top - margin.bottom;
 
-  var cellSize = Math.min(containerWidth, containerHeight) / N;
 
   var circle_data = [];
   var line_data = [];
-  var text_data = []; // Added missing text_data array
+
 
   // Generate data for each cell in the grid
   for (var i = 0; i < N; i++) {
@@ -79,14 +94,22 @@ function motion2(num) {
         cx: cx,
         cy: cy,
         id: id,
-        dx: radius,
+        dx: cellSize/2,
         x_diff: 0, // No horizontal movement
         y_diff: 1, // Vertical movement direction (always downward)
         speed: speed,
         move: 0
       });
 
-      if (j === 0) {
+      box_data_2.push({
+        x: j * cellSize,
+        y: i * cellSize,
+        h: cellSize,
+        w: cellSize
+      });
+
+      //Vertical Axis
+     /* if (j === 0) {
         line_data.push({
           x1: j * cellSize,
           x2: j * cellSize,
@@ -100,13 +123,33 @@ function motion2(num) {
         y2: (i + 0.92) * cellSize
       });
 
-      text_data.push({
-        x: cx - 12,
-        y: cy,
-        value: id + 1
-      });
+    
     }
+  
+*/
   }
+}
+  var box_2 = svg
+    .selectAll(".rect")
+    .data(box_data_2)
+    .enter()
+    .append("rect")
+    .attr("class", "rect")
+    .style("fill", "white")
+    .attr("stroke", "black")
+    .attr("stroke-width", 0.5)
+    .attr("x", function(d) {
+      return d.x;
+    })
+    .attr("y", function(d) {
+      return d.y;
+    })
+    .attr("height", function(d) {
+      return d.h;
+    })
+    .attr("width", function(d) {
+      return d.w;
+    });
 
   var lines = svg
     .selectAll(".line")
@@ -144,24 +187,73 @@ function motion2(num) {
       return d.cy;
     });
 
-  var text = svg
-    .selectAll(".text")
-    .data(text_data)
-    .enter()
-    .append("text")
-    .attr("class", "text")
-    .attr("x", function (d) {
-      return d.x;
-    })
-    .attr("y", function (d) {
-      return d.y;
-    })
-    .text(function (d) {
-      return d.value;
-    })
-    .style("text-anchor", "middle")
-    .style("alignment-baseline", "middle")
-    .style("fill", "black");
+  // Create arrow markers
+  svg
+  .append("defs")
+  .append("marker")
+  .attr("id", "arrowhead")
+  .attr("refX", 0) // Set refX to half of the cell size
+  .attr("refY", 3)
+  .attr("markerWidth", 30)
+  .attr("markerHeight", 30)
+  .attr("orient", "auto")
+  .append("path")
+  .attr("d", "M 0,0 V 6 L9,3 Z")
+  .attr("fill", "black");
+
+// Function to add an arrow to a specific cell
+// Function to add an arrow to a specific cell
+function addArrow(row, col) {
+  var cellSize = maxCellSize;
+  var cx = (col + 0.5) * cellSize;
+  var cy = (row + 0.5) * cellSize;
+
+  // Calculate the arrow positions based on circle radius
+  var circleRadius = radius; // Set the circle radius as the default
+
+  if (row >= 0 && row < N && col >= 0 && col < N) {
+    circleRadius = circle_data[row * N + col].dx;
+  }
+
+  // Define the endpoint for the arrow (the middle of the outside border)
+  var arrowStartX, arrowStartY, arrowEndX, arrowEndY;
+
+  if (col === 0) {
+    arrowStartX = -cellSize / 2;
+    arrowEndX = cx - circleRadius - 15; // Adjusted to avoid overlap with circle
+  } else if (col === N - 1) {
+    arrowStartX = totalGridWidth + cellSize / 2;
+    arrowEndX = cx + circleRadius + 15; // Adjusted to avoid overlap with circle
+  } else {
+    arrowStartX = arrowEndX = cx;
+  }
+
+  if (row === 0) {
+    arrowStartY = -cellSize / 2;
+    arrowEndY = cy - circleRadius - 15; // Adjusted to avoid overlap with circle
+  } else if (row === N - 1) {
+    arrowStartY = totalGridHeight + cellSize / 2;
+    arrowEndY = cy + circleRadius + 15; // Adjusted to avoid overlap with circle
+  } else {
+    arrowStartY = arrowEndY = cy;
+  }
+
+  // Add an arrow line
+  svg
+    .append("line")
+    .attr("x1", arrowStartX)
+    .attr("y1", arrowStartY)
+    .attr("x2", arrowEndX)
+    .attr("y2", arrowEndY)
+    .attr("stroke", "black")
+    .attr("stroke-width", 1.5)
+    .attr("marker-end", "url(#arrowhead)");
+}
+// Add arrows to cells [0, 2] and [2, 0]
+addArrow(0, 2);
+addArrow(2, 0);
+
+
 
   var t;
   t = d3.timer(animate);
@@ -169,16 +261,15 @@ function motion2(num) {
   count = 1;
   svg.style("opacity", 1);
 
-  console.log("Vertical");
 
   function animate(elapsed) {
     circles.attr("cy", function (d) {
-      d.cy = d.cy + d.speed * m_s1 * d.y_diff;
+      d.cy = d.cy + d.speed * m_s1 * d.y_diff ;
 
       // Adjust the condition for top and bottom boundary
       var row = Math.floor(d.id / N); // Calculate the row index
-      var topBoundary = (row + 0.2) * cellSize;
-      var bottomBoundary = (row + 0.8) * cellSize;
+      var topBoundary = (row + 0.25) * cellSize; // Adjust these values for your desired range
+      var bottomBoundary = (row + 0.75) * cellSize; // Adjust these values for your desired range
 
       if (d.cy <= topBoundary || d.cy >= bottomBoundary) {
         d.y_diff = d.y_diff * -1;
