e2b62b60ed16a774e91cdf7920227e158e81e50e
Reza <reza.sh44@gmail.com>
2023-09-25 09:09:19 -0400

    Updated Position!

--- scripts/position_encoding.js
+++ scripts/position_encoding.js
@@ -1,220 +1,259 @@
-var margin = { top: 100, right: 5, bottom: 100, left: 100 };
-var width = 350;
-var height = 250;
+var ID;
+var Value;
+var radio1;
+var radio2;
+var results_json = [];
+var current_elem = 0;
+var array_elem1 = 0;
+var array_elem2 = 0;
+var array_elem3 = 0;
+var motion_type = [1, 2, 3];
+var move_true;
+var count;
+var padding = 10;
+var timer_ret_val = false;
 
-var dataXRange = { min: 0, max: 100 };
-var dataYRange = { min: 0, max: 0 };
-var xAxisLabelHeader = "";
-var yAxisLabelHeader = "Start";
-var circleRadius = 10;
+var margin = {
+  top: 18,
+  left: 18,
+  bottom: 18,
+  right: 18
+};
 
-var data;
-var chart;
-var chartWidth;
-var chartHeight;
-init(drawDots);
+var chartWidth = document.getElementById("chartContainer").offsetWidth - margin.left - margin.right;
+var chartHeight = document.getElementById("chartContainer").offsetHeight - margin.top - margin.bottom;
 
-function init(drawChart) {
-  console.log("initiating!");
-  chartWidth = width - margin.left - margin.right;
-  chartHeight = height - margin.top - margin.bottom;
-  data = [
-    {
-      id: "0",
-      item: "1",
-      xVal: "20",
-      yVal: "0"
-    },
-    {
-      id: "1",
-      item: "2",
-      xVal: (40).toString(),
-      yVal: "0"
-    }
+var svg = d3
+  .select("#chart")
+  .append("svg")
+  .attr("width", chartWidth)
+  .attr("height", chartHeight)
+  .append("g")
+  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+var display1 = d3.select("#chart");
+var display2 = d3.select("#test");
+var display3 = d3.select("#ranger");
+var display4 = d3.select("#last");
+var display5 = d3.select("#notice");
+// Added by Shae
+var display6 = d3.select("#test_difficulty");
+var display7 = d3.select("#ranger_difficulty");
+
+var N = 10; // Change N to the desired size of the grid
+
+motion4(array_elem3, N); // Pass the N value to the function
+
+function motion4(num, N) {
+  var circle_data_2 = [];
+  var box_data_2 = [];
+
+  var gridWidth = chartWidth - margin.left - margin.right; // Width of the grid area
+  var gridHeight = chartHeight - margin.top - margin.bottom; // Height of the grid area
+
+  var maxCellSize = Math.min(gridWidth, gridHeight) / 10; // Determine the maximum cell size for N = 10
+  var cellSize = maxCellSize; // Use this as the fixed cell size for all values of N
+
+  var totalGridWidth = cellSize * N;
+  var totalGridHeight = cellSize * N;
+
+  var translateX = (chartWidth - totalGridWidth) / 2;
+  var translateY = (chartHeight - totalGridHeight) / 2;
+  svg.attr("transform", "translate(" + translateX + "," + translateY + ")");
+
+  // Define an array of arrays to store x positions for each row of cells
+  var xPositions = [
+    [50, 100, 200, 300, 350, 450, 500, 600, 680, 750], // Row 1
+    [50, 100, 200, 300, 350, 450, 500, 600, 680, 750], // Row 2
+    [50, 100, 200, 300, 350, 450, 500, 600, 680, 750], // Row 3
+    [50, 100, 200, 300, 350, 450, 500, 600, 680, 750], // Row 4
+    [50, 100, 200, 300, 350, 450, 500, 600, 680, 750], // Row 5
+    [50, 100, 200, 300, 350, 450, 500, 600, 680, 750], // Row 6
+    [50, 100, 200, 300, 350, 450, 500, 600, 680, 750], // Row 7
+    [50, 100, 200, 300, 350, 450, 500, 600, 680, 750], // Row 8
+    [50, 100, 200, 300, 350, 450, 500, 600, 680, 750], // Row 9
+    [50, 100, 200, 300, 350, 450, 500, 600, 680, 750]  // Row 10
   ];
-  console.log(data);
-  initializeChart();
-  createAxes();
-  drawChart();
-} //end init
 
-function initializeChart() {
-  chart = d3
-    .select("#chart")
-    .append("svg")
-    .attr("width", width)
-    .attr("height", height);
+  // Generate circle and box data based on grid size N
+  for (var i = 0; i < N; i++) {
+    for (var j = 0; j < N; j++) {
+      var cx = xPositions[i][j]; // X position of circle center
+      var cy = (i + 0.5) * cellSize; // Y position of circle center
 
-  chart.plotArea = chart
-    .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-}
+      var radius = cellSize * 0.1; // Adjust the radius based on cell size
 
-function createAxes() {
-  // x axis
-  chart.xScale = d3
-    .scaleLinear()
-    .domain([dataXRange.min, dataXRange.max])
-    .range([0, chartWidth]);
+      var Values = [1]; // Set a constant value of 1 for all circles
+      var value = Values[0]; // Get the value from the array (constant for all circles)
 
-  chart.xAxis = d3
-    .axisBottom()
-    .tickSize(0)
-    .scale(chart.xScale);
+      circle_data_2.push({
+        x: cx, // Use the specified x position
+        y: cy,
+        id: i * N + j + 1,
+        r_diff: 0.13,
+        radius: radius,
+        move: 0,
+        value: value
+      });
 
-  // chart.xAxisContainer = chart
-  //   .append("g")
-  //   .attr("class", "x axis scatter-xaxis")
-  //   .attr(
-  //     "transform",
-  //     "translate(" + margin.left + ", " + (chartHeight + margin.top) + ")"
-  //   )
-  //   .call(chart.xAxis)
-  //   .selectAll("text")
-  //   .remove();
-
-  // x axis header label
-  // chart
-  //   .append("text")
-  //   .attr("class", "x axis scatter-xaxis")
-  //   .style("font-size", "12px")
-  //   .attr("text-anchor", "middle")
-  //   .attr(
-  //     "transform",
-  //     "translate(" +
-  //       (margin.left + chartWidth / 2.0) +
-  //       ", " +
-  //       (chartHeight + margin.bottom / 2.0) +
-  //       ")"
-  //   )
-  //   .text(xAxisLabelHeader);
-
-  // y axis labels
-  chart.yScale = d3
-    .scaleLinear()
-    .domain([dataYRange.min, dataYRange.max])
-    .range([chartHeight, 0]);
-
-  chart.yAxis = d3
-    .axisLeft()
-    .tickSize(0)
-    .scale(chart.yScale);
-
-  chart.yAxisContainer = chart
-    .append("g")
-    .attr("class", "y axis scatter-yaxis")
-    .attr("transform", "translate(" + margin.left + ", " + margin.top + ")")
-    .call(chart.yAxis)
-    .selectAll("text")
-    .remove();
-
-  // y axis header label
-  chart
-    .append("text")
-    .style("font-size", "18px")
-    .attr("class", "heatmap-yaxis")
-    .attr("text-anchor", "end")
-    .attr(
-      "transform",
-      `translate(${margin.left / 1.1},${chartHeight / 0.45}), rotate(-90)`
-      // "translate(" +
-      //   margin.left / 1.1 +
-      //   ", " +
-      //   chartHeight / 0.5 +
-      //   ") rotate(-90)"
-    )
-    .text(yAxisLabelHeader);
-}
-
-function drawDots() {
-  // do something with the data here!
-
-  // plot dots
-  var dots = chart.plotArea
-    .selectAll(".dot")
-    .data(data)
-    .enter()
-    .append("circle")
-    .attr("class", "dot")
-    .attr("cx", function(d) {
-      return chart.xScale(d.xVal);
-    })
-    .attr("cy", function(d) {
-      return chart.yScale(d.yVal);
-    })
-    .attr("r", circleRadius)
-    .attr("fill", "#008fb3")
-    .attr("text", function(d) {
-      d.item;
-    })
-    .on("click", function(d) {
-      console.log("circle: ", d.xVal, ", ", d.yVal);
-      changeBox(d3.select(this).attr("cx"), chart);
-    })
-    .on("mouseover", function(d) {
-      d3.select(this).attr("fill", "red");
-    })
-    .on("mouseout", function(d) {
-      d3.select(this).attr("fill", "blue");
-    });
-
-  chart.plotArea
-    .selectAll(".text")
-    .data(data)
-    .enter()
-    .append("text")
-    .attr("class", "label")
-    .attr("x", function(d) {
-      return chart.xScale(d.xVal) - 4;
-    })
-    .attr("y", function(d) {
-      return chart.yScale(d.yVal);
-    })
-    .attr("dy", "2.75em")
-    .text(function(d) {
-      return d.item;
-    });
-}
-
-function drawBoxes() {
-  var box = chart.plotArea
-    .selectAll(".box")
-    .data(data)
-    .enter()
-    .append("rect")
-    .attr("class", "rect")
-    .attr("x", function(d) {
-      return chart.xScale(d.xVal);
-    })
-    .attr("y", function(d) {
-      return chart.yScale(d.yVal);
-    })
-    .attr("width", 5)
-    .attr("height", 5)
-    .attr("fill", "green")
-    .on("click", function(d) {
-      console.log("circle: ", d.xVal, ", ", d.yVal);
-      changeDot(d3.select(this).attr("x"), chart);
-    })
-    .on("mouseover", function(d) {
-      d3.select(this).attr("fill", "red");
-    })
-    .on("mouseout", function(d) {
-      d3.select(this).attr("fill", "green");
-    });
-}
-function changeDot(x, chart) {
-  chart.plotArea.selectAll(".dot").each(function(point) {
-    if (d3.select(this).attr("cx") == x) {
-      d3.select(this.attr("fill", "yellow"));
+      box_data_2.push({
+        x: j * cellSize,
+        y: i * cellSize,
+        h: cellSize,
+        w: cellSize
+      });
     }
+  }
+  
+
+  // Create arrow markers
+  svg
+    .append("defs")
+    .append("marker")
+    .attr("id", "arrowhead")
+    .attr("refX", 0) // Set refX to half of the cell size
+    .attr("refY", 3)
+    .attr("markerWidth", 30)
+    .attr("markerHeight", 30)
+    .attr("orient", "auto")
+    .append("path")
+    .attr("d", "M 0,0 V 6 L9,3 Z")
+    .attr("fill", "black");
+
+  // Function to add an arrow to a specific cell
+  function addArrow(row, col) {
+    var cellSize = maxCellSize ;
+    var cx = (col + 0.5) * cellSize;
+    var cy = (row + 0.5) * cellSize;
+    var circleRadius = circle_data_2[row * N + col].radius; // Get the circle's radius
+
+    // Define the endpoint for the arrow (the middle of the outside border)
+    var arrowStartX, arrowStartY, arrowEndX, arrowEndY;
+
+    if (col === 0) {
+      arrowStartX = -cellSize / 2;
+      arrowEndX = cx - circleRadius - 20; // Adjusted to avoid overlap with circle
+  } else if (col === N - 1) {
+      arrowStartX = totalGridWidth + cellSize / 2;
+      arrowEndX = cx + circleRadius + 20; // Adjusted to avoid overlap with circle
+  } else {
+      arrowStartX = arrowEndX = cx;
+  }
+
+  if (row === 0) {
+      arrowStartY = -cellSize / 2;
+      arrowEndY = cy - circleRadius - 20; // Adjusted to avoid overlap with circle
+  } else if (row === N - 1) {
+      arrowStartY = totalGridHeight + cellSize / 2;
+      arrowEndY = cy + circleRadius + 20; // Adjusted to avoid overlap with circle
+  } else {
+      arrowStartY = arrowEndY = cy;
+  }
+
+  var box_2 = svg
+  .selectAll(".rect")
+  .data(box_data_2)
+  .enter()
+  .append("rect")
+  .attr("class", "rect clickable") // Add 'clickable' class
+  .style("fill", "white")
+  .attr("stroke", "black")
+  .attr("stroke-width", 0.5)
+  .attr("x", function(d) {
+    return d.x;
+  })
+  .attr("y", function(d) {
+    return d.y;
+  })
+  .attr("height", function(d) {
+    return d.h;
+  })
+  .attr("width", function(d) {
+    return d.w;
   });
+
+// Event listener for cell clicks to highlight the corresponding circle
+box_2.on("click", function(d, i) {
+  var clickedCircle = circles_2.nodes()[i];
+  if (selectedCircle === clickedCircle) {
+    // If the same circle is clicked again, unselect it
+    d3.select(clickedCircle).attr("stroke", "none");
+    selectedCircle = null;
+  } else {
+    // Unselect the previously selected circle (if any)
+    if (selectedCircle) {
+      d3.select(selectedCircle).attr("stroke", "none");
+    }
+    // Highlight the corresponding circle
+    d3.select(clickedCircle).attr("stroke", "black").attr("stroke-width", 2);
+    selectedCircle = clickedCircle;
+  }
+});
+
+// Add a class to the circles to make them clickable
+var circles_2 = svg
+  .selectAll(".circle")
+  .data(circle_data_2)
+  .enter()
+  .append("circle")
+  .attr("class", "circle clickable") // Add 'clickable' class
+  .attr("r", function(d) {
+    return d.radius; // Use the adjusted radius
+  })
+  .attr("fill", "#008fb3")
+  .attr("cx", function(d) {
+    return d.x;
+  })
+  .attr("cy", function(d) {
+    return d.y;
+  });
+
+var selectedCircle = null; // To keep track of the selected circle
+
+// Event listener for left-click to highlight the cell
+circles_2.on("click", function(d) {
+  if (selectedCircle === d) {
+    // If the same circle is clicked again, unselect it
+    d3.select(this).attr("stroke", "none");
+    selectedCircle = null;
+  } else {
+    // Unselect the previously selected circle (if any)
+    if (selectedCircle) {
+      d3.select(selectedCircle).attr("stroke", "none");
+    }
+    // Highlight the clicked circle
+    d3.select(this).attr("stroke", "black").attr("stroke-width", 2);
+    selectedCircle = this;
+  }
+});
+
+// Event listener for right-click anywhere to unselect the cell
+d3.select("body").on("contextmenu", function() {
+  if (selectedCircle) {
+    d3.select(selectedCircle).attr("stroke", "none");
+    selectedCircle = null;
+  }
+  d3.event.preventDefault(); // Prevent the default right-click context menu
+});
+
+    // Add an arrow line
+    svg
+        .append("line")
+        .attr("x1", arrowStartX)
+        .attr("y1", arrowStartY)
+        .attr("x2", arrowEndX)
+        .attr("y2", arrowEndY)
+        .attr("stroke", "black")
+        .attr("stroke-width", 1.5)
+        .attr("marker-end", "url(#arrowhead)");
 }
 
-function changeBox(x, chart) {
-  chart.plotArea.selectAll("rect").each(function(point) {
-    if (d3.select(this).attr("x") == x) {
-      d3.select(this).attr("fill", "yellow");
-    }
-  });
+  // Add arrows to cells [0, 2] and [2, 2]
+  addArrow(2, 0);
+  addArrow(0, 1);
+
+  
+
+  move_true = true;
+  console.log("area");
 }
