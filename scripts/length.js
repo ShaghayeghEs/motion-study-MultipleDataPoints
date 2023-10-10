d6bba30afba92335b698622fdc9dab1f0d41ae21
ShaghayeghEs <shaghayegh.esmaeili.1993@gmail.com>
2023-10-10 02:34:28 -0400

    core.js added with functions for distributions, etc.

--- scripts/length.js
+++ scripts/length.js
@@ -1,53 +1,110 @@
-// var ID;
-// var Value;
-// var radio1;
-// var radio2;
-// var results_json = [];
-// var current_elem = 0;
-// var array_elem1 = 0;
-// var array_elem2 = 0;
-// var array_elem3 = 0;
-// var motion_type = [1, 2, 3];
-// var move_true;
-// var count;
-// var padding = 10;
-// var timer_ret_val = false;
+// Import the functions from core.js
+import { mapValues, arrayToMatrix, randomizeArrayWithFixedIndices,
+  findOuterPairsWithDistance, selectDistArray } from './core.js';
 
-// // Generating a sample of data with a uniform distribution
-// function generateUniformData(min, max, count) {
-//   var data = [];
-//   var step = (max - min) / (count - 1);
-//   for (var i = 0; i < count; i++) {
-//     data.push(min + step * i);
-//   }
-//   return data;
-// }
 
-// // Example usage:
-// var min = 1;     // Minimum value
-// var max = 100;   // Maximum value
-// var count = 100;  // Number of data points
-// var uniformData = generateUniformData(min, max, count);
+var iCell1 = 0;
+var jCell1 = 0;
+var iCell2 = 0;
+var jCell2 = 0;
 
-// console.log(uniformData);
+//function to shuffle the original array based on task and ratio
+function shuffleArray(arr, task, ratio) {
+  console.log("arr is: ");
+  console.log (arr);
+  
+  var shuffledArray =[];
+  
+  if (task == "max" || task == "min") {
+    // Create a copy of the input array to avoid modifying the original array
+    shuffledArray = arr.slice();
 
-var uniform_3 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-var uniform_10 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18,
-   19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37,
-    38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56,
-     57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75,
-      76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94,
-       95, 96, 97, 98, 99, 100];
+    // Fisher-Yates shuffle algorithm
+    for (let i = shuffledArray.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]];
+    } 
+  }
+  else if (task == "compare") {
+    var pairs = [];
+    if (N == 3) {
+      pairs = findOuterPairsWithDistance(3,3);} //finding possible cells to be used in the "compare" task
+    else if (N == 10) {
+      pairs = findOuterPairsWithDistance(10,5); //TODO Later
+    }
 
-var leftSkewed_3 = [1.00, 1.00, 1.00, 1.01, 1.39, 2.43, 3.01, 6.03, 8.50];
-// var leftSkewed_3 = [1.00, 1.20, 1.46, 1.95, 2.58, 3.99, 5.75, 7.32, 8.84]; //version 2
-var leftSkewed_10 = [1.00, 1.00, 1.00, 1.00, 1.00, 1.01, 1.01, 1.01, 1.03, 1.04, 1.05, 1.09, 1.17, 1.21,
-   1.23, 1.24, 1.26, 1.54, 1.57, 1.66, 1.69, 1.77, 1.78, 1.81, 1.90, 2.11, 2.31, 2.60, 3.12, 3.24, 3.48,
-    3.94, 4.44, 4.50, 4.51, 4.98, 5.07, 5.77, 6.53, 6.78, 6.79, 7.08, 7.19, 7.34, 7.66, 9.91, 10.04,
-     10.23, 10.91, 11.21, 11.88, 12.61, 14.46, 14.51, 14.78, 16.22, 17.04, 17.66, 19.15, 19.57, 20.46,
-      25.80, 28.23, 28.48, 30.42, 31.36, 32.24, 32.73, 32.75, 33.83, 33.91, 35.09, 36.16, 38.62, 40.10,
-       40.31, 41.60, 45.97, 46.50, 46.81, 47.65, 47.74, 48.67, 49.33, 50.43, 50.89, 53.68, 54.91, 59.60,
-        62.48, 65.04, 65.28, 67.31, 70.65, 84.11, 92.31, 94.05, 97.25, 98.93, 99.28];
+    const randomElement = pairs[Math.floor(Math.random() * pairs.length)]; //randomly selecting one of the pairs
+    // Access the coordinates (i and j) of cell1 in the pair
+    // const iCell1 = randomElement.cell1[0];
+    // const jCell1 = randomElement.cell1[1];
+    iCell1 = randomElement.cell1[0];
+    jCell1 = randomElement.cell1[1];
+    const index1 = (iCell1 * N) + jCell1; //index in the 1D array
+
+    // Access the coordinates (i and j) of cell2 in the pair
+    // const iCell2 = randomElement.cell2[0];
+    // const jCell2 = randomElement.cell2[1];
+    iCell2 = randomElement.cell2[0];
+    jCell2 = randomElement.cell2[1];
+    const index2 = (iCell2 * N) + jCell2; //index in the 1D array
+
+
+    // TEST
+    console.log(`Cell 1: i=${iCell1}, j=${jCell1}`);
+    console.log(`Cell 2: i=${iCell2}, j=${jCell2}`);
+    console.log(`index1: ${index1}, index2:${index2}`);
+
+    var indexValue;
+    var indexRatio;
+
+    if (N == 3) {
+      if (dist == "uniform"){
+        indexValue = arr.indexOf(10);
+        indexRatio = arr.indexOf(10 * ratio);}
+      else {
+        indexValue = arr.indexOf(20);
+        indexRatio = arr.indexOf(20 * ratio);
+      }
+    } else if (N == 10) {
+        //TODO LATER
+    }
+    
+
+    // TEST
+    console.log(`index of value: ${indexValue}, ${indexRatio}`);
+
+    //TODO LATER BUG
+    //TEST arr = [20, 30, 35, 40, 45, 47, 48, 50, 50]
+    //TEST index1 = 5 index2 = 0
+    //TEST indexValue = 0 indexRatio = 7
+
+    //WRONG
+    // [arr[indexValue], arr[index1]] = [arr[index1], arr[indexValue]];
+    // [arr[indexRatio], arr[index2]] = [arr[index2], arr[indexRatio]];
+
+    //TODO LATER: make it a function
+    var temp;
+    temp = arr[indexValue];
+    arr[indexValue] = arr[index1];
+    arr[index1] = temp;
+
+    temp = arr[indexRatio];
+    arr[indexRatio] = arr[index2];
+    arr[index2] = temp;
+
+    //Test
+    console.log("swapped test array:")
+    console.log(arr);
+
+    shuffledArray = randomizeArrayWithFixedIndices(arr, index1, index2);
+
+    //TEST
+    console.log("shuffled after swap test array:")
+    console.log(shuffledArray);
+
+  }
+  return shuffledArray;
+}
 
 var margin = {
   top: 18,
@@ -67,19 +124,16 @@ var svg = d3
   .append("g")
   .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-// var display1 = d3.select("#chart");
-// var display2 = d3.select("#test");
-// var display3 = d3.select("#ranger");
-// var display4 = d3.select("#last");
-// var display5 = d3.select("#notice");
-// Added by Shae
-// var display6 = d3.select("#test_difficulty");
-// var display7 = d3.select("#ranger_difficulty");
+// var N = url_data["size"]; // size of the grid
+var dist = url_data["dist"]; // distribution of data points
+var ratio_value = url_data["ratio"]; // ratio for compare task, value for max/min
+var N = 3; // test
+var task = url_data["task"]; // type of task
 
-var N = url_data["size"]; // Change N to the desired size of the grid
+//TEST
+console.log(N, task, dist, ratio_value);
+drawLengthGraph(N); // Pass the N value to the function
 
-// drawLengthGraph(array_elem3, N); 
-drawLengthGraph(N) // Pass the N value to the function
 
 function drawLengthGraph(N) {
   var box_data_2 = [];
@@ -97,20 +151,32 @@ function drawLengthGraph(N) {
   var translateY = (chartHeight - totalGridHeight) / 2;
   svg.attr("transform", "translate(" + translateX + "," + translateY + ")");
 
+  var cellHeights = selectDistArray(dist,N,ratio_value);
+  console.log("original array"); //test
+  console.log(cellHeights); //test
+  cellHeights = shuffleArray(cellHeights,task,ratio_value); //shuffling the data array based on the given task
+  console.log("shuffled array"); //test
+  console.log(cellHeights); //test
+  cellHeights = mapValues(cellHeights, Math.min(...cellHeights), //map the data array to the encoding max and min
+   Math.max(...cellHeights),5,(maxCellSize - 4.7));
+  cellHeights = arrayToMatrix(cellHeights, N); //convert the data 1D array to a matrix
+  console.log("mapped array in a matrix"); //test
+  console.log(cellHeights); //test
+  
   // Specify the height values for each bar chart in the format [h1, h2, h3, ...]
-  var cellHeights = [
-    [5, 85, 15, 20, 25, 30, 40, 50, 60, 70],
-    [5, 10, 15, 20, 25, 30, 40, 50, 60, 70],
-    [5, 10, 15, 20, 25, 30, 40, 50, 60, 70],
-    [5, 10, 15, 20, 25, 30, 40, 50, 60, 70],
-    [5, 10, 15, 20, 25, 30, 40, 50, 60, 70],
-    [5, 10, 15, 20, 25, 30, 40, 50, 60, 70],
-    [5, 10, 15, 20, 25, 30, 40, 50, 60, 70],
-    [5, 10, 15, 20, 25, 30, 40, 50, 60, 70],
-    [5, 10, 15, 20, 25, 30, 40, 50, 60, 70],
-    [5, 10, 15, 20, 25, 30, 40, 50, 60, 70],
-    // Add more rows of heights here...
-  ];
+  // var cellHeights = [
+  //   [5, 85, 15, 20, 25, 30, 40, 50, 60, 70],
+  //   [5, 10, 15, 20, 25, 30, 40, 50, 60, 70],
+  //   [5, 10, 15, 20, 25, 30, 40, 50, 60, 70],
+  //   [5, 10, 15, 20, 25, 30, 40, 50, 60, 70],
+  //   [5, 10, 15, 20, 25, 30, 40, 50, 60, 70],
+  //   [5, 10, 15, 20, 25, 30, 40, 50, 60, 70],
+  //   [5, 10, 15, 20, 25, 30, 40, 50, 60, 70],
+  //   [5, 10, 15, 20, 25, 30, 40, 50, 60, 70],
+  //   [5, 10, 15, 20, 25, 30, 40, 50, 60, 70],
+  //   [5, 10, 15, 20, 25, 30, 40, 50, 60, 70],
+  //   // Add more rows of heights here...
+  // ];
   // Generate box data based on grid size N
   for (var i = 0; i < N; i++) {
     for (var j = 0; j < N; j++) {
@@ -167,6 +233,7 @@ function drawLengthGraph(N) {
     .each(drawCellWithBarChart);
     
   // Create arrow markers
+  if (task == "compare") {
   svg
     .append("defs")
     .append("marker")
@@ -179,6 +246,7 @@ function drawLengthGraph(N) {
     .append("path")
     .attr("d", "M 0,0 V 6 L9,3 Z")
     .attr("fill", "black");
+  }
 
     var selectedRect = null; // To keep track of the selected circle
 
@@ -249,12 +317,10 @@ function drawLengthGraph(N) {
       .attr("stroke-width", 1.5)
       .attr("marker-end", "url(#arrowhead)");
   }
-  
 
-// Create arrows for specific cells
-addArrow(2, 0);
-addArrow(0, 1);
-  
-  move_true = true;
-  console.log("area");
+  // Create arrows for specific cells
+  if (task == "compare") {
+    addArrow(iCell1, jCell1);
+    addArrow(iCell2, jCell2);
+  }
 }
